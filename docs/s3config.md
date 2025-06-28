7e568f57a20a4acaaf4ad3f562cbc8df99114928
makethumb <sourabhs701@gmail.com>
2025-06-28 21:05:38 +0530

    config: s3Config

---
+++ docs/s3config.md
@@ -0,0 +1,39 @@
+## Bucket Policy
+
+```json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::<bucketName>/*"
+    }
+  ]
+}
+```
+
+## Cross-origin resource sharing (CORS)
+
+```json
+[
+  {
+    "AllowedHeaders": ["*"],
+    "AllowedMethods": ["PUT", "POST", "GET", "HEAD"],
+    "AllowedOrigins": [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://custom_domain.com"
+    ],
+    "ExposeHeaders": [
+      "ETag",
+      "x-amz-server-side-encryption",
+      "x-amz-request-id",
+      "x-amz-id-2"
+    ],
+    "MaxAgeSeconds": 3000
+  }
+]
+```
--- src/s3.js
+++ src/s3.js
@@ -7,13 +7,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_S3_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
-const BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 const UPLOAD_EXPIRES_IN = 3600;
 
 export const s3Helper = {
@@ -40,15 +40,15 @@ export const s3Helper = {
     }
   },
   generateFileKey: (userId, filename) => {
-  const timestamp = Date.now();
-  const sanitizedFilename = filename.replace(/[^a-zA-Z0-9.-]/g, "_");
+    const timestamp = Date.now();
+    const sanitizedFilename = filename.replace(/[^a-zA-Z0-9.-]/g, "_");
 
-  const hash = crypto
-    .createHash("sha1")
-    .update(`${userId}-${sanitizedFilename}-${timestamp}`)
-    .digest("hex");
+    const hash = crypto
+      .createHash("sha1")
+      .update(`${userId}-${sanitizedFilename}-${timestamp}`)
+      .digest("hex");
 
     const extension = sanitizedFilename.split(".").pop();
-    return `/${userId}/${hash}.${extension}`;
+    return `${userId}/${hash}.${extension}`;
   },
 };
